b3b53d25a1cbed869914615189317bac7927321f
addis <betsega23@gmail.com>
2023-01-18 15:00:23 +0300

    Add README

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import messagesReducer  from '../features/message/messageSlice';
+import { fetchMessages } from '../features/message/messageSlice';
 
 export const store = configureStore({
   reducer: {
     message: messagesReducer,
   },
 });
+
+store.dispatch(fetchMessages())
 
 export default store;
